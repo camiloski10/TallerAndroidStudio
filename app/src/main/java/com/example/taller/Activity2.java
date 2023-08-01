d6f087066b34d08273046b68d006ac22c211b35f
camiloski10 <camilocp282@gmail.com>
2023-08-01 11:59:24 -0500

    feat::regresarbtn

--- app/src/main/java/com/example/taller/Activity2.java
+++ app/src/main/java/com/example/taller/Activity2.java
@@ -2,13 +2,26 @@ package com.example.taller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
+    Button regresarbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        regresarbtn = findViewById(R.id.Regresar);
+        Intent regresar = new Intent(Activity2.this, MainActivity.class);
+        regresarbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(regresar);
+
+            }
+        });
     }
 }
