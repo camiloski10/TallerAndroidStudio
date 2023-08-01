caab57c01ad6c7c11ff61485196d2e51ea4fd45d
camiloski10 <camilocp282@gmail.com>
2023-08-01 11:42:59 -0500

    first commit

---
+++ app/src/main/java/com/example/taller/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.taller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button siguientebtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        siguientebtn = findViewById(R.id.siguiente);
+        Intent siguientey = new Intent(this, Activity2.class);
+
+        siguientebtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                startActivity(siguientey);
+            }
+        });
+    }
+}
